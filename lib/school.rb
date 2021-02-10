413c50dceb374d9e9d44651b4e2e5276e88d617d
Rene <renegarcia1864@gmail.com>
2021-02-10 14:13:34 -0600

    commit tree 5be91cce648bce7cc3c49e0b27e4a05285ee15fb

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,24 @@
 # code here!
-class School
-attr_accessor :roster
+class School 
+    attr_accessor :roster
+
     def initialize(model)
-      @roster = {}
+        @roster = {} #everytime we create a School.new we create a "model(argument)" with an empty roster 
     end
-    def add_student(name, grade)
-        @roster[grade] ||= []
-        @roster[grade] << name
+
+    def add_student(name, grade) # Having this method adds a "Key" grade is the key
+        @roster[grade] ||= [] # we are creating a new key "grade" and assigning by using "Or equals to"
+        @roster[grade] << name #we are pushing the student name into the new key."grade"
     end
+
     def grade(number)
         @roster[number]
     end
+
     def sort
         hash = {}
-        @roster.keys.sort.each do |number|
-            hash[number] = @roster[number].sort
+        @roster.keys.sort.select do |number|
+        hash[number] = @roster[number].sort
         end
         hash
     end
